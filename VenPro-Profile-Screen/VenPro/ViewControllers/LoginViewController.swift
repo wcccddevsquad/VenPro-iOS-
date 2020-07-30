e7d051ac4352ed831f9c0ac934ff77e8ec430162
Henry <hfeil9768512mail.wcccd.edu>
2020-07-29 22:49:41 -0400

    added FirebaseAuth

--- VenPro-Profile-Screen/VenPro/ViewControllers/LoginViewController.swift
+++ VenPro-Profile-Screen/VenPro/ViewControllers/LoginViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import FirebaseUI
+import FirebaseAuth
 
 
 class LoginViewController: UIViewController, AuthUIDelegate {
@@ -16,12 +17,27 @@ class LoginViewController: UIViewController, AuthUIDelegate {
     
     @IBOutlet weak var userNumber: UITextField!
     
+    lazy var phoneNumber = "+1\(String(describing: userNumber.text))"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     @IBAction func SignupButtonPressed(_ sender: Any) {
         
         self.performSegue(withIdentifier: "GoToAccountCreationScreen", sender: self)
     }
     
     @IBAction func LoginButtonPressed(_ sender: Any) {
+        
+        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error) in
+//   if let error = error {
+//    self.showMessagePrompt(error.localizedDescription)
+//        return
+//   }
+   // Sign in using the verificationID and the code sent to the user
+   // ...
+ }
         
        self.performSegue(withIdentifier: "LoggedIn", sender: self)
 }
