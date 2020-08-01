da47bbc858e119f81d2a5d27ce9a86c8cfc6ec38
Ricky <Rxswasey@gmail.com>
2020-08-01 12:44:14 -0400

    Added Pod For Menu and implemented menu for the Profile Screen

--- VenPro-Profile-Screen/VenPro/ViewControllers/ProfileViewController.swift
+++ VenPro-Profile-Screen/VenPro/ViewControllers/ProfileViewController.swift
@@ -5,11 +5,16 @@
 //  Created by Fariha Hussain on 7/8/20.
 //  Copyright © 2020 VenPro. All rights reserved.
 //
-
+import SideMenu
 import UIKit
+
+
 
 class ProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var menu: SideMenuNavigationController?
+    
+    
     @IBOutlet weak var nickName: UITextField!
     
     @IBOutlet weak var aboutMeField: UITextView!
@@ -49,9 +54,49 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        menu = SideMenuNavigationController(rootViewController: MenuListController())
+        menu?.leftSide = true
+        menu?.setNavigationBarHidden(true, animated: false)
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func didTapMenu() {
+    present(menu!, animated: true)
+    }
 
 }
 
+class MenuListController: UITableViewController {
+    //Controls the list of items in the menu
+    
+    var items = ["first", "Second"]
+    
+    let darkColor = UIColor(red: 33/255.0,
+    green: 33/255.0,
+    blue: 33/255.0,
+    alpha: 1)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = darkColor
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = darkColor
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
