e1b828561fe4f4817acabe593c8950744e2fc59a
wcccddevsquad <67932936+wcccddevsquad@users.noreply.github.com>
2020-12-09 19:35:09 -0500

    Add files via upload

---
+++ VenPro/ViewControllers/CheckoutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CheckoutViewController.swift
+//  VenPro
+//
+//  Created by India Doria on 11/4/20.
+//  Copyright © 2020 VenPro. All rights reserved.
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+
+class CheckoutViewController: UIViewController {
+    
+    
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         // Do any additional setup after loading the view.
+     }
+}
---
+++ VenPro/ViewControllers/DrinkDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DrinkDetailsViewController.swift
+//  VenPro
+//
+//  Created by India Doria on 11/4/20.
+//  Copyright © 2020 VenPro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class DrinkDetailsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
